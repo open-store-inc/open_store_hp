f9a9e755a355059aa9b5cf5d5c4238e6de88037a
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-03-13 12:00:11 +0900

    修正

---
+++ workflow.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Template Name: workflow.php
+*/
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php get_header(); ?> 
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/workflow.css" type="text/css" />
+</head>
+
+<body>
+
+    <div class="header-color">
+        <p></p>
+    </div>
+    <header>
+
+        <div class="header container ">
+            <div class="header-left">
+                <div class="header-logo">
+                    <a href="">OPEN STORE</a>
+                </div>
+
+            </div>
+            <div class="header-right">
+                <div class="header-contact">
+
+                </div>
+                <div class="header-nav" style="width:100px;">
+                    <a href="#" id="header-course"><img
+                            src="https://everydayicons.jp/wp/wp-content/themes/everydayicons/icons/png/ei-menu.png"
+                            alt=""> </a>
+                    <ul id="header-toggle">
+                        <li>初級</li>
+                        <li>中級</li>
+                        <li>上級</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container clear">
+        <div class="top">
+            <div class="top-logo">
+                <h3><span>WORKFLOW</span></h3>
+                　　　<p>ワークフロー</p>
+            </div>
+            <div class="top-me">
+                <p>
+                    <br>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="workflow_content1">
+    </div>
+    <div class="workflow_content2">
+    </div>
+    <div class="workflow_content3">
+    </div>
+    <div class="workflow_content4">
+    </div>
+    <div class="workflow_content5">
+    </div>
+    <div class="workflow_content6">
+    </div>
+</body>
